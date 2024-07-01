d51d2790856d467c32683d89492e725e1fbe66f8
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 04:23:26 +0530

    Nightly Update

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config()
-const express=require('express')
-const cors=require('cors')
-const morgan=require("morgan")
-const cookieParser=require("cookie-parser")
-const authRoutes=require("./routes/Auth")
-const productRoutes=require("./routes/Product")
-const orderRoutes=require("./routes/Order")
-const cartRoutes=require("./routes/Cart")
-const brandRoutes=require("./routes/Brand")
-const categoryRoutes=require("./routes/Category")
-const userRoutes=require("./routes/User")
-const addressRoutes=require('./routes/Address')
-const reviewRoutes=require("./routes/Review")
-const wishlistRoutes=require("./routes/Wishlist")
+const express = require('express')
+const cors = require('cors')
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+const authRoutes = require("./routes/Auth")
+const productRoutes = require("./routes/Product")
+const orderRoutes = require("./routes/Order")
+const cartRoutes = require("./routes/Cart")
+const brandRoutes = require("./routes/Brand")
+const categoryRoutes = require("./routes/Category")
+const userRoutes = require("./routes/User")
+const addressRoutes = require('./routes/Address')
+const reviewRoutes = require("./routes/Review")
+const wishlistRoutes = require("./routes/Wishlist")
 const { connectToDB } = require("./database/db")
 
 
@@ -32,6 +32,9 @@ const corsOptions = {
 
 server.use(cors(corsOptions));
 
+app.options("*", cors({ origin: '*', optionsSuccessStatus: 200 }));
+
+
 
 server.use(express.json());
 server.use(cookieParser());
@@ -41,21 +44,34 @@ server.use(morgan("tiny"));
 
 
 // routeMiddleware
-server.use("/auth",authRoutes)
-server.use("/users",userRoutes)
-server.use("/products",productRoutes)
-server.use("/orders",orderRoutes)
-server.use("/cart",cartRoutes)
-server.use("/brands",brandRoutes)
-server.use("/categories",categoryRoutes)
-server.use("/address",addressRoutes)
-server.use("/reviews",reviewRoutes)
-server.use("/wishlist",wishlistRoutes)
+server.use("/auth", authRoutes)
+server.use("/users", userRoutes)
+server.use("/products", productRoutes)
+server.use("/orders", orderRoutes)
+server.use("/cart", cartRoutes)
+server.use("/brands", brandRoutes)
+server.use("/categories", categoryRoutes)
+server.use("/address", addressRoutes)
+server.use("/reviews", reviewRoutes)
+server.use("/wishlist", wishlistRoutes)
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true"); // Add this line to allow credentials
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH,,OPTIONS"); // Update to match the allowed methods of your API
+    res.header("Access-Control-Expose-Headers", "Content-Length, X-Total-Count,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-MD5, Content-Type, Date, X-Api-Version"); // Add this line to expose headers you want to return
+    res.header("Access-Control-Max-Age", "86400"); // Set cache control to 24 hours
+    res.header("Access-Control-Request-Headers", "Content-Type, Authorization"); // Add this line to allow headers you want to send in the request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    next();
+});
 
-
-server.get("/",(req,res)=>{
-    res.status(200).json({message:'running11'})
+server.get("/", (req, res) => {
+    res.status(200).json({ message: 'Server is running' })
 })
 
 server.get("/testcors", (req, res) => {
@@ -66,6 +82,6 @@ server.get("/testcors", (req, res) => {
 });
 
 
-server.listen(process.env.PORT || 8000,()=>{
-    console.log('server [STARTED] ~ http://localhost: '+process.env.PORT);
+server.listen(process.env.PORT || 8000, () => {
+    console.log('server [STARTED] ~ http://localhost: ' + process.env.PORT);
 })
