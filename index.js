16c8ef6b043a4200ea988014dfe8b6c2c85c1577
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-01 05:39:19 +0530

    udpate CORS

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ connectToDB()
 
 
 // middlewares
-server.use(cors({origin: 'https://frontend-iota-beryl-58.vercel.app/',credentials:true,exposedHeaders:['X-Total-Count'],methods:['GET','POST','PATCH','DELETE']}))
+server.use(cors({origin: "https://frontend-iota-beryl-58.vercel.app",credentials:true,exposedHeaders:['X-Total-Count'],methods:['GET','POST','PATCH','DELETE']}))
 server.use(express.json())
 server.use(cookieParser())
 server.use(morgan("tiny"))
