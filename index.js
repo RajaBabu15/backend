4579893e9a63d984c2d3e0607de7e0378a53cbfd
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 05:20:32 +0530

    Eding the edge

--- index.js
+++ index.js
@@ -27,12 +27,13 @@ const corsOptions = {
     origin: '*', // Allow requests from any origin
     credentials: true, // Allow cookies to be sent with requests
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allow these HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these header
+    optionsSuccessStatus: 200, // Set the status code for CORS preflight requests
 };
 
 server.use(cors(corsOptions));
 
-server.options("*", cors({ origin: '*'}));
+server.options("*", cors({ origin: '*',"optionsSuccessStatus": 200}));
 
 
 
@@ -59,14 +60,15 @@ server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true"); // Add this line to allow credentials
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH,,OPTIONS"); // Update to match the allowed methods of your API
-    res.header("Access-Control-Expose-Headers", "Content-Length, X-Total-Count,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-MD5, Content-Type, Date, X-Api-Version"); // Add this line to expose headers you want to return
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH,OPTIONS"); // Update to match the allowed methods of your API
+    res.header("Access-Control-Expose-Headers", "'X-Api-Version', 'Accept-Version', 'Content-MD5', 'Access-Control-Request-Headers', 'Date', 'X-Total-Count', 'Access-Control-Request-Method', 'X-CSRF-Token', 'X-API-KEY', 'Origin', 'Content-Type', 'Authorization', 'Accept', 'Content-Length', 'X-Requested-With"); // Add this line to expose headers you want to return
     res.header("Access-Control-Max-Age", "86400"); // Set cache control to 24 hours
     res.header("Access-Control-Request-Headers", "Content-Type, Authorization"); // Add this line to allow headers you want to send in the request
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
     }
+    res.status(200); 
     next();
 });
 
