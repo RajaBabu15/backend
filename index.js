0abd88c1ffae19707404eba10ce76aa307802a8a
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-01 07:25:35 +0530

    update1

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ server.use("/wishlist",wishlistRoutes)
 
 
 server.get("/",(req,res)=>{
-    res.status(200).json({message:'running'})
+    res.status(200).json({message:'running11'})
 })
 
-server.get("/test-cors", (req, res) => {
+server.get("/testcors", (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
