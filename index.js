f6229b2a21324d10f5edd144ec232bf98ed839e3
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 23:21:27 +0530

    sndofasodflmasldmdflmaslskdmflkmsadlfmlkasmdlfkmasldmflkmsfmunjya

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ server.use(cors({origin:'https://frontend-iota-beryl-58.vercel.app',credentials:
 server.use(express.json())
 server.use(cookieParser())
 server.use(morgan("tiny"))
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://frontend-iota-beryl-58.vercel.app');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // routeMiddleware
 server.use("/auth",authRoutes)
