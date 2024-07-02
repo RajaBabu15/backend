8ac467f9745e08e6d897c7554cb0690b0466873c
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 07:39:54 +0530

    raweraew

--- index.js
+++ index.js
@@ -1,63 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/Auth');
-const productRoutes = require('./routes/Product');
-const orderRoutes = require('./routes/Order');
-const cartRoutes = require('./routes/Cart');
-const brandRoutes = require('./routes/Brand');
-const categoryRoutes = require('./routes/Category');
-const userRoutes = require('./routes/User');
-const addressRoutes = require('./routes/Address');
-const reviewRoutes = require('./routes/Review');
-const wishlistRoutes = require('./routes/Wishlist');
-const { connectToDB } = require('./database/db');
+require("dotenv").config()
+const express=require('express')
+const cors=require('cors')
+const morgan=require("morgan")
+const cookieParser=require("cookie-parser")
+const authRoutes=require("./routes/Auth")
+const productRoutes=require("./routes/Product")
+const orderRoutes=require("./routes/Order")
+const cartRoutes=require("./routes/Cart")
+const brandRoutes=require("./routes/Brand")
+const categoryRoutes=require("./routes/Category")
+const userRoutes=require("./routes/User")
+const addressRoutes=require('./routes/Address')
+const reviewRoutes=require("./routes/Review")
+const wishlistRoutes=require("./routes/Wishlist")
+const { connectToDB } = require("./database/db")
+
 
 // server init
-const server = express();
+const server=express()
 
 // database connection
-connectToDB();
+connectToDB()
+
 
 // middlewares
-const corsOptions = {
-    origin: '*', // Allow requests from this origin
-    credentials: true, // Allow cookies to be sent with requests
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allow these HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-};
-
-server.use(cors(corsOptions));
-server.use(express.json());
-server.use(cookieParser());
-server.use(morgan('tiny'));
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Restrict to required domain
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Custom-Header');
-    next();
-});
+server.use(cors({credentials:true,exposedHeaders:['X-Total-Count'],methods:['GET','POST','PATCH','DELETE']}))
+server.use(express.json())
+server.use(cookieParser())
+server.use(morgan("tiny"))
 
 // routeMiddleware
-server.use('/auth', authRoutes);
-server.use('/users', userRoutes);
-server.use('/products', productRoutes);
-server.use('/orders', orderRoutes);
-server.use('/cart', cartRoutes);
-server.use('/brands', brandRoutes);
-server.use('/categories', categoryRoutes);
-server.use('/address', addressRoutes);
-server.use('/reviews', reviewRoutes);
-server.use('/wishlist', wishlistRoutes);
+server.use("/auth",authRoutes)
+server.use("/users",userRoutes)
+server.use("/products",productRoutes)
+server.use("/orders",orderRoutes)
+server.use("/cart",cartRoutes)
+server.use("/brands",brandRoutes)
+server.use("/categories",categoryRoutes)
+server.use("/address",addressRoutes)
+server.use("/reviews",reviewRoutes)
+server.use("/wishlist",wishlistRoutes)
 
-server.get('/', (req, res) => {
-    res.status(200).json({ message: 'Server is running' });
-});
 
-server.listen(process.env.PORT || 8000, () => {
-    console.log('server [STARTED] ~ http://localhost:' + (process.env.PORT || 8000));
-});
+
+server.get("/",(req,res)=>{
+    res.status(200).json({message:'running'})
+})
+
+server.listen(8000,()=>{
+    console.log('server [STARTED] ~ http://localhost:8000');
+})
