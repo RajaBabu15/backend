3b7b2f2955cbc99106510b96c20d9ddecccfcde2
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 05:36:30 +0530

    Login Logconsole.log("CORS preflight request

--- index.js
+++ index.js
@@ -57,18 +57,30 @@ server.use("/reviews", reviewRoutes)
 server.use("/wishlist", wishlistRoutes)
 
 server.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true"); // Add this line to allow credentials
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH,OPTIONS"); // Update to match the allowed methods of your API
-    res.header("Access-Control-Expose-Headers", "'X-Api-Version', 'Accept-Version', 'Content-MD5', 'Access-Control-Request-Headers', 'Date', 'X-Total-Count', 'Access-Control-Request-Method', 'X-CSRF-Token', 'X-API-KEY', 'Origin', 'Content-Type', 'Authorization', 'Accept', 'Content-Length', 'X-Requested-With"); // Add this line to expose headers you want to return
-    res.header("Access-Control-Max-Age", "86400"); // Set cache control to 24 hours
-    res.header("Access-Control-Request-Headers", "Content-Type, Authorization"); // Add this line to allow headers you want to send in the request
+    console.log("req",req);
+    console.log(req.method, req.url);
+    // res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Credentials", "true"); // Add this line to allow credentials
+    // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH,OPTIONS"); // Update to match the allowed methods of your API
+    // res.header("Access-Control-Expose-Headers", "'X-Api-Version', 'Accept-Version', 'Content-MD5', 'Access-Control-Request-Headers', 'Date', 'X-Total-Count', 'Access-Control-Request-Method', 'X-CSRF-Token', 'X-API-KEY', 'Origin', 'Content-Type', 'Authorization', 'Accept', 'Content-Length', 'X-Requested-With"); // Add this line to expose headers you want to return
+    // res.header("Access-Control-Max-Age", "86400"); // Set cache control to 24 hours
+    // res.header("Access-Control-Request-Headers", "Content-Type, Authorization"); // Add this line to allow headers you want to send in the request
     if (req.method === 'OPTIONS') {
-        res.status(200).end();
+        res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Credentials", "true"); // Add this line to allow credentials
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH,OPTIONS"); // Update to match the allowed methods of your API
+        res.header("Access-Control-Expose-Headers", "'X-Api-Version', 'Accept-Version', 'Content-MD5', 'Access-Control-Request-Headers', 'Date', 'X-Total-Count', 'Access-Control-Request-Method', 'X-CSRF-Token', 'X-API-KEY', 'Origin', 'Content-Type', 'Authorization', 'Accept', 'Content-Length', 'X-Requested-With"); // Add this line to expose headers you want to return
+        res.header("Access-Control-Max-Age", "86400"); // Set cache control to 24 hours
+        res.header("Access-Control-Request-Headers", "Content-Type, Authorization"); // Add this line to allow headers you want to send in the request
+    
+        res.status(200).json({"message": "CORS preflight request"});
         return;
     }
     res.status(200); 
+
+    console.log("res",res);
     next();
 });
 
